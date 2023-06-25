d47567d753d364c589f52f3f5f44b8bca9a5dda1
Chip Collier <photex@gmail.com>
2023-06-25 09:48:47 +0200

    Ok, we know how to determine which mode we're running in

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,13 @@
 const std = @import("std");
+const path = std.fs.path;
+const hash = std.hash;
 const assert = std.debug.assert;
+
+const zigBinHash = hash.Crc32.hash("zig");
+
+fn zig_mode() !void {}
+
+fn ziege_mode() !void {}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -12,7 +20,17 @@ pub fn main() !void {
     var args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
-    for (args, 0..) |arg, index| {
+    std.debug.print("CMD: {s}\n", .{args[0]});
+
+    const binName = path.basename(args[0]);
+
+    if (hash.Crc32.hash(binName) == zigBinHash) {
+        std.debug.print("We are running in zig mode!\n", .{});
+    } else {
+        std.debug.print("We are running in goat mode.\n", .{});
+    }
+
+    for (args[1..], 1..) |arg, index| {
         std.debug.print("{d}: {s}\n", .{ index, arg });
     }
 }
