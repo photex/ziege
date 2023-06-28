d95f32c77a4185c6924e003e757b5610162c46fe
Chip <photex@gmail.com>
2023-06-28 11:24:17 +0200

    turns out there is standard logging already

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const log = std.log;
 const path = std.fs.path;
 const hash = std.hash;
 const mem = std.mem;
@@ -8,6 +9,7 @@ const assert = std.debug.assert;
 
 const Mode = enum {
     Zig,
+    Zls,
     Ziege
 };
 
@@ -19,11 +21,11 @@ const App = struct {
     mode: Mode,
 
     pub fn init(allocator: Allocator) !App {
-        std.debug.print("Initializing.\n", .{});
-
+        log.debug("Initializing...", .{});
         var args = try std.process.argsAlloc(allocator);
 
-        const zigBinNameHash = hash.Crc32.hash("zig");
+        const zigBinNameHash = comptime hash.Crc32.hash("zig");
+        const zlsBinNameHash = comptime hash.Crc32.hash("zls");
 
         const binName = path.basename(args[0]);
         const binNameHash = hash.Crc32.hash(binName);
@@ -31,29 +33,42 @@ const App = struct {
         return App {
             .allocator = allocator,
             .args = args,
-            .mode = if (binNameHash == zigBinNameHash) .Zig else .Ziege,
+            .mode = switch (binNameHash) {
+                zigBinNameHash => .Zig,
+                zlsBinNameHash => .Zls,
+                else => .Ziege,
+            },
         };
     }
 
     pub fn deinit(self: *App) !void {
-        std.debug.print("Shutting down.\n", .{});
+        log.debug("Cleaning up...", .{});
         std.process.argsFree(self.allocator, self.args);
     }
 };
 
+// For args that start with '+' we interpret as arguments
+// for us rather than the tools we proxy.
 fn parse_args(app: App) !void {
-    for (app.args[1..], 1..) |arg, index| {
-        std.debug.print("{d}: {s}\n", .{ index, arg });
+    for (app.args[1..]) |arg| {
+        if (arg[0] == '+') {
+            log.info("Found an arg for ziege: {s}", .{arg});
+        }
     }
 }
 
 fn zig_mode(app: App) !void {
-    std.debug.print("We are running in zig mode!\n", .{});
+    log.debug("We are running in zig mode!", .{});
+    try parse_args(app);
+}
+
+fn zls_mode(app: App) !void {
+    log.debug("We are running in zls mode!", .{});
     try parse_args(app);
 }
 
 fn ziege_mode(app: App) !void {
-    std.debug.print("We are running in goat mode.\n", .{});
+    log.debug("We are running in goat mode.", .{});
     try parse_args(app);
 }
 
@@ -65,10 +80,11 @@ pub fn main() !void {
     }
 
     var app = try App.init(gpa.allocator());
-    defer app.deinit() catch @panic("WTF");
+    defer app.deinit() catch @panic("Unrecoverable error during shutdown!");
 
     switch (app.mode) {
         .Zig => try zig_mode(app),
+        .Zls => try zls_mode(app),
         .Ziege => try ziege_mode(app),
     }
 }
