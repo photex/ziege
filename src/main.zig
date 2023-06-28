3db8699a95925ef064d999bb47af07d89551e365
Chip <photex@gmail.com>
2023-06-28 12:29:21 +0200

    Logging, that's scoped.

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-const log = std.log;
 const path = std.fs.path;
 const hash = std.hash;
 const mem = std.mem;
@@ -15,61 +14,80 @@ const Mode = enum {
 
 const Args = [][:0]u8;
 
-const App = struct {
-    allocator: Allocator,
-    args: Args,
-    mode: Mode,
+fn define_app() type {
+    return struct {
+        const Self = @This();
+        const log = std.log.scoped(.ziege);
 
-    pub fn init(allocator: Allocator) !App {
-        log.debug("Initializing...", .{});
-        var args = try std.process.argsAlloc(allocator);
+        allocator: Allocator,
+        args: Args,
+        mode: Mode,
 
-        const zigBinNameHash = comptime hash.Crc32.hash("zig");
-        const zlsBinNameHash = comptime hash.Crc32.hash("zls");
+        pub fn init(allocator: Allocator) !Self {
+            Self.log.debug("Initializing...", .{});
+            var args = try std.process.argsAlloc(allocator);
 
-        const binName = path.basename(args[0]);
-        const binNameHash = hash.Crc32.hash(binName);
+            const zigBinNameHash = comptime hash.Crc32.hash("zig");
+            const zlsBinNameHash = comptime hash.Crc32.hash("zls");
 
-        return App {
-            .allocator = allocator,
-            .args = args,
-            .mode = switch (binNameHash) {
-                zigBinNameHash => .Zig,
-                zlsBinNameHash => .Zls,
-                else => .Ziege,
-            },
-        };
-    }
+            const binName = path.basename(args[0]);
+            const binNameHash = hash.Crc32.hash(binName);
 
-    pub fn deinit(self: *App) !void {
-        log.debug("Cleaning up...", .{});
-        std.process.argsFree(self.allocator, self.args);
-    }
-};
+            return Self {
+                .allocator = allocator,
+                .args = args,
+                .mode = switch (binNameHash) {
+                    zigBinNameHash => .Zig,
+                    zlsBinNameHash => .Zls,
+                    else => .Ziege,
+                },
+            };
+        }
+
+        pub fn deinit(self: *Self) !void {
+            Self.log.debug("Cleaning up...", .{});
+            std.process.argsFree(self.allocator, self.args);
+        }
+    };
+}
+const App = define_app();
 
 // For args that start with '+' we interpret as arguments
 // for us rather than the tools we proxy.
-fn parse_args(app: App) !void {
+fn extract_args(app: App) !void {
     for (app.args[1..]) |arg| {
         if (arg[0] == '+') {
-            log.info("Found an arg for ziege: {s}", .{arg});
+            App.log.debug("Found an arg for ziege: {s}", .{arg});
         }
     }
 }
 
 fn zig_mode(app: App) !void {
-    log.debug("We are running in zig mode!", .{});
-    try parse_args(app);
+    App.log.debug("We are running in zig mode!", .{});
+    try extract_args(app);
+
+    var zig = std.ChildProcess.init(
+        &.{"/home/chip/.local/bin/zig", "help"},
+        app.allocator);
+
+    try zig.spawn();
+
+    App.log.debug("spawned {d}", .{zig.id});
+
+    const term = try zig.wait();
+    if (term != .Exited) {
+        App.log.err("There was an error running zig.", .{});
+    }
 }
 
 fn zls_mode(app: App) !void {
-    log.debug("We are running in zls mode!", .{});
-    try parse_args(app);
+    App.log.debug("We are running in zls mode!", .{});
+    try extract_args(app);
 }
 
 fn ziege_mode(app: App) !void {
-    log.debug("We are running in goat mode.", .{});
-    try parse_args(app);
+    App.log.debug("We are running in goat mode.", .{});
+    try extract_args(app);
 }
 
 pub fn main() !void {
