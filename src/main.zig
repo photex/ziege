752431e684df7c70edc8125a4bb8083dba8d6349
Chip Collier <photex@gmail.com>
2024-06-01 22:30:00 +0200

    slowly getting some thoughts together

--- src/main.zig
+++ src/main.zig
@@ -41,25 +41,6 @@ const json_platform = arch ++ "-" ++ os;
 const archive_ext = if (builtin.os.tag == .windows) "zip" else "tar.xz";
 const home_var = if (builtin.os.tag == .windows) "USERPROFILE" else "HOME";
 
-/// Read the contents of the file specified by file_path and return a u8 slice with it's contents.
-fn readFile(allocator: Allocator, file_path: []const u8) ![]u8 {
-    var file = try std.fs.openFileAbsolute(file_path, .{});
-    defer file.close();
-    return try file.readToEndAlloc(allocator, std.math.maxInt(usize));
-}
-
-/// Load a zig release index
-fn loadToolchainIndex(allocator: Allocator) !json.Parsed(json.Value) {
-    log.warn("**TODO** toolchain index is still only read from disk at cwd.", .{});
-    const index_text = try readFile(allocator, "zig_index.json");
-    return try json.parseFromSlice(std.json.Value, allocator, index_text, .{});
-}
-
-fn findTargetZigVersion(allocator: Allocator) ![]u8 {
-    log.debug("**TODO** zig toolchain version is only search for in cwd.", .{});
-    return try readFile(allocator, ".zigversion");
-}
-
 /// For args that start with '+' we interpret as arguments
 /// for us rather than the tools we proxy.
 fn extract_args(allocator: Allocator, launcher_args: *ArgList, forward_args: *ArgList) !void {
@@ -71,13 +52,11 @@ fn extract_args(allocator: Allocator, launcher_args: *ArgList, forward_args: *Ar
 
     // We preserve the path to the executable that got run in the launcher
     // args so that we can figure out what mode to operate in etc.
-    const launcher = try allocator.allocSentinel(u8, args[0].len, 0);
-    @memcpy(launcher, args[0]);
-    try launcher_args.append(launcher);
+    const binpath = try allocator.dupeZ(u8, args[0]);
+    try launcher_args.append(binpath);
 
     for (args[1..]) |arg| {
-        const copy = try allocator.allocSentinel(u8, arg.len, 0);
-        @memcpy(copy, arg);
+        const copy = try allocator.dupeZ(u8, arg);
         if (arg[0] == '+') {
             try launcher_args.append(copy);
         } else {
@@ -90,26 +69,32 @@ const Locations = struct {
     const Self = @This();
     home: []u8,
     config: []u8,
-    toolchains: []u8,
+    zig_pkgs: []u8,
+    zls_pkgs: []u8,
 
     pub fn init(allocator: Allocator) !Self {
         const home = try std.process.getEnvVarOwned(allocator, home_var);
-        const config = try std.fs.path.join(allocator, &.{ home, ".zig" });
-        const toolchains = try std.fs.path.join(allocator, &.{ config, "toolchains" });
+        const config = try std.fs.path.join(allocator, &.{ home, ".ziege" });
+        const zig_pkgs = try std.fs.path.join(allocator, &.{ config, "pkg", "zig" });
+        const zls_pkgs = try std.fs.path.join(allocator, &.{ config, "pkg", "zls" });
 
-        log.debug("CONFIG: {s}", .{config});
-        log.debug("TOOLCHAINS: {s}", .{toolchains});
+        log.debug("ZIEGE ROOT: {s}", .{config});
+        log.debug("ZIG PACKAGES: {s}", .{zig_pkgs});
+        log.debug("ZLS PACKAGES: {s}", .{zls_pkgs});
+
         return Self{
             .home = home,
             .config = config,
-            .toolchains = toolchains,
+            .zig_pkgs = zig_pkgs,
+            .zls_pkgs = zls_pkgs,
         };
     }
 
     pub fn deinit(self: *Self, allocator: Allocator) void {
         allocator.free(self.home);
         allocator.free(self.config);
-        allocator.free(self.toolchains);
+        allocator.free(self.zig_pkgs);
+        allocator.free(self.zls_pkgs);
     }
 };
 
@@ -141,9 +126,6 @@ const Launcher = struct {
 
         const locations = try Locations.init(allocator);
 
-        // const index = try loadToolchainIndex(allocator);
-        // _ = index;
-
         return Self{
             .allocator = allocator,
             .launcher_args = launcher_args,
@@ -155,10 +137,36 @@ const Launcher = struct {
 
     pub fn deinit(self: *Self) !void {
         log.debug("Cleaning up...", .{});
-        //std.process.argsFree(self.allocator, self.argv);
         self.locations.deinit(self.allocator);
+
+        for (self.launcher_args.items) |arg| {
+            self.allocator.free(arg);
+        }
         self.launcher_args.deinit();
+
+        for (self.forward_args.items) |arg| {
+            self.allocator.free(arg);
+        }
         self.forward_args.deinit();
+    }
+
+    /// Read the contents of the file specified by file_path and return a u8 slice with it's contents.
+    fn readFile(self: *Self, file_path: []const u8) ![]u8 {
+        var file = try std.fs.openFileAbsolute(file_path, .{});
+        defer file.close();
+        return try file.readToEndAlloc(self.allocator, std.math.maxInt(usize));
+    }
+
+    /// Load a zig release index
+    fn loadZigReleaseIndex(self: *Self) !json.Parsed(json.Value) {
+        log.warn("**TODO** toolchain index is still only read from disk at cwd.", .{});
+        const index_text = try readFile(self.allocator, "zig_index.json");
+        return try json.parseFromSlice(std.json.Value, self.allocator, index_text, .{});
+    }
+
+    fn findTargetZigVersion(allocator: Allocator) ![]u8 {
+        log.debug("**TODO** zig toolchain version is only search for in cwd.", .{});
+        return try readFile(allocator, ".zigversion");
     }
 };
 
