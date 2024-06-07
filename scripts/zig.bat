2b41230532ca85c3f52d6a4d82c9b3ed0f4559f5
Chip Collier <photex@gmail.com>
2024-06-07 15:59:23 +0200

    Adding batch scripts for cmd.exe users

---
+++ scripts/zig.bat
@@ -0,0 +1,6 @@
+rem This script is to accomodate you if you're a user of cmd.exe instead of powershell.
+rem Copy it somewhere in %PATH% (ideally next to where you've put ziege.exe).
+
+@echo off
+
+ziege +zig %*
