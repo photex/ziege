5b9de7a9ac45f51e41152f43fd3898e7c6234cf6
Chip Collier <photex@gmail.com>
2024-06-10 15:11:32 +0200

    Local release script buttoned up

--- .builds/build_release.sh
+++ .builds/build_release.sh
@@ -4,10 +4,17 @@ set -e
 
 # Build all release targets
 
-zig build -Doptimize=ReleaseFast -Dtarget=x86_64-linux -Dcpu=x86_64+avx -p _release/linux/x86_64
-zig build -Doptimize=ReleaseFast -Dtarget=x86_64-macos -Dcpu=x86_64+avx -p _release/macos/x86_64
-zig build -Doptimize=ReleaseFast -Dtarget=aarch64-linux -p _release/linux/aarch64
-zig build -Doptimize=ReleaseFast -Dtarget=aarch64-macos -p _release/macos/aarch64
+zig build -Doptimize=ReleaseFast -Dtarget=x86_64-linux -Dcpu=x86_64+avx -p _release/build/linux/x86_64
+zig build -Doptimize=ReleaseFast -Dtarget=x86_64-macos -Dcpu=x86_64+avx -p _release/build/macos/x86_64
+zig build -Doptimize=ReleaseFast -Dtarget=aarch64-linux -p _release/build/linux/aarch64
+zig build -Doptimize=ReleaseFast -Dtarget=aarch64-macos -p _release/build/macos/aarch64
 
-zig build -Doptimize=ReleaseFast -Dtarget=x86_64-windows -Dcpu=x86_64+avx -p _release/windows/x86_64
-cp scripts/*.bat _release/windows/x86_64/bin/.
+zig build -Doptimize=ReleaseFast -Dtarget=x86_64-windows -Dcpu=x86_64+avx -p _release/build/windows/x86_64
+
+zip -j _release/ziege-windows-x86_64.zip _release/build/windows/x86_64/bin/ziege.exe scripts/zig.bat scripts/zls.bat
+cp _release/build/linux/x86_64/bin/ziege _release/ziege-linux-x86_64
+cp _release/build/macos/x86_64/bin/ziege _release/ziege-macos-x86_64
+cp _release/build/linux/aarch64/bin/ziege _release/ziege-linux-aarch64
+cp _release/build/macos/aarch64/bin/ziege _release/ziege-macos-aarch64
+
+rm -rf _release/build
